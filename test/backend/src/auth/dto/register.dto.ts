51413d8259a52cc52f607222a76444734b3d39bc
Nur-akhmad <89442612+AkhmadR12@users.noreply.github.com>
2025-03-28 11:03:34 +0700

    Add files via upload

---
+++ test/backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, IsNumber, IsArray } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+ 
+}
+  
